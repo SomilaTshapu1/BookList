04058b43ecae97d33563809717ed5041dff4a7b1
user <user@DESKTOP-62TOGLF>
2023-01-11 23:40:11 +0200

    Add project files.

---
+++ BookList/Pages/Booklist/Datatables.cshtml.cs
@@ -0,0 +1,39 @@
+using BookList.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookList.Pages.Booklist
+{
+    public class DatatablesModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DatatablesModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Datatables");
+        }
+    }
+}
